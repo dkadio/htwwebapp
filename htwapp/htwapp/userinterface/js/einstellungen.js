13c4b86a27a81e4a48594c9862df10d31c0357c3
dkadio <d.kadio@gmail.com>
2013-12-24 18:51:02 +0100

    htw app service db alles clear

---
+++ htwapp/htwapp/userinterface/js/einstellungen.js
@@ -0,0 +1,50 @@
+﻿$(document).ready(function () {
+    //studiengang und semster vorbelegen
+    setValues();
+
+
+    $('#studiengang').bind('change', function () {
+        var $this = $(this),
+            val = $this.val();
+        localStorage.setItem("studiengang", val);
+    });
+    $('#semester').bind('change', function () {
+        var $this = $(this),
+            val = $this.val();
+        localStorage.setItem("semester", val);
+    });
+
+    //wahlfaehcer
+    setLS();
+});
+
+function setLS() {
+    
+    var loadUrl = "http://localhost:52142/htwservice.ashx?function=" + "wahlfaecher" + "";
+
+    $("#wahlfach")
+        .load(loadUrl, null, function (responseText) {
+            $("#wahlfach").html(responseText);
+            //TODO wahlfächer refreshen.
+            $("#wahlfach").page();
+        });
+}
+
+
+function setValues() {
+    // selectfelder abholen
+    var se = $('#semester');
+    var sg = $('#studiengang')
+    // localstorage werte in das selectfeld laden und alle anderen abwaehlen
+    se.val(localStorage.getItem("semester")).attr('selected', true).siblings('option').removeAttr('selected');
+    sg.val(localStorage.getItem("studiengang")).attr('selected', true).siblings('option').removeAttr('selected');
+    // initialisieren der menues
+    se.selectmenu();
+    sg.selectmenu();
+    // ui refresh
+    se.selectmenu("refresh", true);
+    sg.selectmenu("refresh", true);
+}
+
+
+
---
+++ htwapp/htwapp/userinterface/js/viewfach.js
@@ -0,0 +1,24 @@
+﻿/*$(document).ready(function () {
+    alert("viewfach");
+});
+
+*/
+function ajaxRequest() {
+    var loadUrl = "http://localhost:52142/htwservice.ashx?function=" + "dozentenlistview";
+
+    $("#dozent")
+        .load(loadUrl, null, function (responseText) {
+            $("#dozent").html(responseText);
+            $("ul").listview("refresh");
+        });
+}
+
+function showfachview(element) {
+    alert("viewfach");
+
+}
+
+
+function getfach() {
+    alert("fachview");
+}
